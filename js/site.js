a25e83646a1ac9e6b13f092c0013126b23a8d11e
Paulo da Silva <pdsprog@gmail.com>
2020-10-20 18:39:52 -0400

    Add About Me Section and update some files

--- js/site.js
+++ js/site.js
@@ -1,8 +1,8 @@
 const site = {
-  mobileButton: document.querySelector('.mobile_menu'),
+  mobileBtn: document.querySelector('.mobile_menu'),
   menuItems: document.querySelectorAll('.header_navbar--item'),
 
-  displayMobileMenu() {
+  displayMenu() {
     if (window.screen.width < 768) {
       const menu = document.querySelector('.header');
       const icon = document.querySelector('.mobile_menu--icon');
@@ -13,8 +13,12 @@ const site = {
   },
 
   init() {
-    this.mobileButton.addEventListener('click', this.displayMobileMenu);
-    this.menuItems.forEach(item => item.addEventListener('click', this.displayMobileMenu));
+    //Mobile Button
+    this.mobileBtn.addEventListener('click', this.displayMenu);
+    this.menuItems.forEach(item => item.addEventListener('click', this.displayMenu));
+
+    //General
+    AOS.init({duration: 700});
   }
 }
 
